e7701e0af51dabb605cf35fae66e84d564b010c8
maxpoole <64996563+maxPoole15@users.noreply.github.com>
2021-11-22 13:40:01 -0700

    Updated Menu

---
+++ Group_Project/Assets/Scripts/objectRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectRotation : MonoBehaviour
+{
+    public float turnSpeed = 10;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+    }
+}
